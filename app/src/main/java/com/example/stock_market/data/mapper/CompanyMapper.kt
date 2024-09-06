fefcebbdd7c724229932784896e1271ffa01fd9c
Wamitinewton <wamitinewton@gmail.com>
2024-09-06 06:48:09 +0300

    listing entities

---
+++ app/src/main/java/com/example/stock_market/data/mapper/CompanyMapper.kt
@@ -0,0 +1,22 @@
+@file:Suppress("UNREACHABLE_CODE")
+
+package com.example.stock_market.data.mapper
+
+import com.example.stock_market.data.local.CompanyListingEntity
+import com.example.stock_market.domain.model.CompanyListing
+
+fun CompanyListingEntity.toCompanyListing(): CompanyListing {
+    return CompanyListing(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+
+    fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity{
+        return CompanyListingEntity(
+            name = name,
+            symbol = symbol,
+            exchange = exchange
+        )
+    }
+}
