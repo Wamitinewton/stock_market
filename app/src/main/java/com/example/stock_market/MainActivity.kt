2667329f4675df87b4365679eb8f0b73dc591905
Wamitinewton <wamitinewton@gmail.com>
2024-09-06 14:40:08 +0300

    company listing view model

--- app/src/main/java/com/example/stock_market/MainActivity.kt
+++ app/src/main/java/com/example/stock_market/MainActivity.kt
@@ -3,14 +3,6 @@ package com.example.stock_market
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.stock_market.ui.theme.StockmarketTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,29 +10,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StockmarketTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StockmarketTheme {
-        Greeting("Android")
-    }
-}
