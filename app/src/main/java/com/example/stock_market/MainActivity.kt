acb9da2d400f0869d3d33132540aefcd6856899e
Wamitinewton <wamitinewton@gmail.com>
2024-09-07 18:33:17 +0300

    company info screen

--- app/src/main/java/com/example/stock_market/MainActivity.kt
+++ app/src/main/java/com/example/stock_market/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import com.example.stock_market.presentation.company_listings.NavGraphs
+import com.example.stock_market.presentation.NavGraphs
 import com.example.stock_market.ui.theme.StockmarketTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
